995291e2f4a0b26ac62310352a1380f4312643d0
elninocavalcanti10 <acn2011@outlook.com>
2019-06-18 11:37:20 -0300

    Variáveis de ambiente

--- src/models/File.js
+++ src/models/File.js
@@ -11,7 +11,14 @@ const File = new mongoose.Schema({
     }
 }, 
 {
-  timestamps: true
+  timestamps: true,
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true }
+});
+
+File.virtual("url").get(function() {
+  const url = process.env.URL || 'http://localhost:3333';
+  return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
 module.exports = mongoose.model("File", File);
